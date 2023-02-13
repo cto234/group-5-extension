26d462b9aa966b52010a1cabb32c0f0263463467
cto234 <cto234@nyu.edu>
2023-02-12 20:20:21 -0500

    Added search functionality

--- src/popup/search.js
+++ src/popup/search.js
@@ -1,20 +1,26 @@
-firefox.contextMenus.create({
-    id: "search-selected-text",
-    title: "Search selected text",
-    contexts: ["selection"],
+function search() {
+  const form = document.getElementById("form");                
+  form.addEventListener("submit", function (event) {            
+    event.preventDefault();                                     //Default action is to open a new page
+    let inputText = document.getElementById("searchBar").value; 
+    inputText = URLify(inputText);
+    console.log(inputText)
+    const url = "https://www.ratemyprofessors.com/search/teachers?query="+ inputText + "&sid=U2Nob29sLTY3NQ=="
+    window.open(url, "_blank");
   });
-  
-  firefox.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "search-selected-text") {
-      searchSelectedText(info.selectionText);
-    }
-  });
-  
-  import data from '../../data/updatedrmp.json';
+}
 
-  function searchSelectedText(selectedText) {
-    firefox.tabs.create({
-      url: "https://www.ratemyprofessors.com/search/teachers?query="+ selectedText + "&sid=U2Nob29sLTY3NQ==",
-      //rating: data[selectedText]
-    });
-  }
+document.addEventListener('DOMContentLoaded', function() {  //Must wait for DOM content to load before anything can be done
+  console.log('DOM content loaded');
+  search()                                                  //Runs search function
+});
+
+function URLify(input)      //Replaces spaces with %20 to make a string into a url slug
+{
+    for(let i=0 ; i<input.length ; i++)
+    {
+        if(input[i] == ' ')
+            input = input.replace(input[i], '%20');
+    }
+    return input;
+}
